a83545ecfa161e6f23998ec297aa2707c4943d12
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2023-05-20 08:50:06 -0400

    quick break

--- create.php
+++ create.php
@@ -1,3 +1,35 @@
+<?php
+$name = "";
+$email = "";
+$phone = "";
+$address = "";
+
+$errorMessege = "";
+$succsessMessege = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+
+    do {
+        if (empty($name || $email || $phone || $address)) {
+            $errorMessege = "All fields are required";
+            break;
+        }
+
+        // add new client to the database
+        $name = "";
+        $email = "";
+        $phone = "";
+        $address = "";
+
+        $succsessMessege = "New client added successfully";
+    } while (false);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,29 +44,45 @@
 <body>
     <div class="container my-5">
         <h2>New Client</h2>
+
+        <?php
+        //break
+        if (!empty($errorMessege)) {
+            echo "<div class='alert alert-danger' role='alert'>
+            $errorMessege
+            </div>";
+        }
+        ?>
+
         <form method="post">
             <div class="row mb-3">
                 <label class="col-sm-3 col-form-label">Name</label>
                 <div class="col-sm-6>
-                        <input type=" text" class="form-control" name="name" value="">
+                        <input type=" text" class="form-control" name="name" value="<?php echo $name; ?>">
                 </div>
                 <div class="row mb-3">
                     <label class="col-sm-3 col-form-label">Email</label>
                     <div class="col-sm-6>
-                        <input type=" text" class="form-control" name="email" value="">
+                        <input type=" text" class="form-control" name="email" value="<?php echo $email; ?>">
                     </div>
                     <div class="row mb-3">
                         <label class="col-sm-3 col-form-label">Phone</label>
                         <div class="col-sm-6>
-                        <input type=" text" class="form-control" name="phone" value="">
+                        <input type=" text" class="form-control" name="phone" value="<?php echo $phone; ?>">
                         </div>
                         <div class="row mb-3">
                             <label class="col-sm-3 col-form-label">Address</label>
                             <div class="col-sm-6>
-                        <input type=" text" class="form-control" name="address" value="">
+                        <input type=" text" class="form-control" name="address" value="<?php echo $address; ?>">
                             </div>
-
-                            <!-- quick break: add a submit and cancel button and make it work with the database-->
+                            <div class="row mb-3">
+                                <div class="offset-sm-3 col-sm-3 d-grid">
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                                <div class="col-sm-3 d-grid">
+                                    <a class="btn btn-outline-secondary" href="/myshop/index.php" role="button">Cancel</a>
+                                </div>
+                            </div>
 
         </form>
     </div>
