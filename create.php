a36910329ef4aa6df0fd379c9a35d2742a294403
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2023-05-19 22:52:24 -0400

    format

--- create.php
+++ create.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,38 +8,40 @@
     <title>My Shop</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container my-5">
         <h2>New Client</h2>
-            <form method="post">
-                <div class="row mb-3">
-                    <label class="col-sm-3 col-form-label">Name</label>
-                    <div class="col-sm-6>
-                        <input type="text" class="form-control" name="name" value="">
+        <form method="post">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label">Name</label>
+                <div class="col-sm-6>
+                        <input type=" text" class="form-control" name="name" value="">
                 </div>
                 <div class="row mb-3">
                     <label class="col-sm-3 col-form-label">Email</label>
                     <div class="col-sm-6>
-                        <input type="text" class="form-control" name="email" value="">
-                </div>
-                <div class="row mb-3">
-                    <label class="col-sm-3 col-form-label">Phone</label>
-                    <div class="col-sm-6>
-                        <input type="text" class="form-control" name="phone" value="">
-                </div>
-                <div class="row mb-3">
-                    <label class="col-sm-3 col-form-label">Address</label>
-                    <div class="col-sm-6>
-                        <input type="text" class="form-control" name="address" value="">
-                </div>
+                        <input type=" text" class="form-control" name="email" value="">
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">Phone</label>
+                        <div class="col-sm-6>
+                        <input type=" text" class="form-control" name="phone" value="">
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-sm-3 col-form-label">Address</label>
+                            <div class="col-sm-6>
+                        <input type=" text" class="form-control" name="address" value="">
+                            </div>
 
-                <!-- quick break: add a submit and cancel button and make it work with the database-->
+                            <!-- quick break: add a submit and cancel button and make it work with the database-->
 
-            </form>
+        </form>
     </div>
 </body>
+
 </html>
 
 <?php
-    phpinfo();
+phpinfo();
 ?>
